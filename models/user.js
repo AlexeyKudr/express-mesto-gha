56c6593236a41f1d4b644faf49740f5384980df0
AlexeyKud <renenkton123@yandex.ru>
2024-01-11 09:59:56 +0300

    Pr14 fix avatars url

--- models/user.js
+++ models/user.js
@@ -37,6 +37,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (link) => validator.isURL(link),
+      message: (props) => `${props.value} неверный формат ссылки`,
+    },
   },
 });
 
